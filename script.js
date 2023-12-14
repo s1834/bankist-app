85a0d25c8725dfd8da1ac49a72e295aad918c4e6
s1834 <s18324@protonmail.com>
2023-12-15 01:07:31 +0530

    added current balance fucntionality

--- script.js
+++ script.js
@@ -74,3 +74,10 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
